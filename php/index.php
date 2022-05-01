6937e40b178d77d97009a398e5b0b01c518f083e
MINAS\User <ics20072@uom.edu.gr>
2022-05-01 18:31:19 +0300

    nothing special

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ require_once "initialize.php";
 // Initialize the session
 session_start();
  
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, if not then redirect him to the login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
