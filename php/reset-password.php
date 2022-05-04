9ceca0f23745f5a4216127d5dbf0b1a89d3106d4
MINAS\User <ics20072@uom.edu.gr>
2022-05-04 18:02:46 +0300

    Centre the reset password wrapper

--- php/reset-password.php
+++ php/reset-password.php
@@ -78,7 +78,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
+        .wrapper{ 
+            width: 360px; 
+            padding: 20px; 
+            margin: auto;
+        }
     </style>
 </head>
 <body>
