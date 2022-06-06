e696a7853f0f1eae87b133eacdb90220e5e6aca2
MINAS\User <ics20072@uom.edu.gr>
2022-06-06 04:35:25 +0300

    small changes to imports

--- Js/controller.js
+++ Js/controller.js
@@ -1,14 +1,11 @@
 import { loadData } from "./Services/loadFromJSON.js";
-import { DATA_PLAYERS_PATH } from "./config.js";
-import { DATA_TEAMS_PATH } from "./config.js";
+import * as PATH from "./config.js";
 import { postToDB } from "./Services/storeToDB.js";
-import { PLAYER_API_PATH } from "./config.js";
-import { TEAM_API_PATH } from "./config.js";
 
 const btnLoadData = document.querySelector("#btn-load-data");
 
 const controlData = function () {
-  loadData(DATA_PLAYERS_PATH).then(function (dataArr) {
+  loadData(PATH.DATA_PLAYERS_PATH).then(function (dataArr) {
     dataArr.forEach(async function (player) {
       const playerObj = {
         //as "id" will have the default auto increment
@@ -19,11 +16,11 @@ const controlData = function () {
         team: Math.floor(Math.random() * 25) + 1, //team id (1 - 25) cause we got 25 teams
         photo: "Undefined",
       };
-      await postToDB(playerObj, PLAYER_API_PATH);
+      await postToDB(playerObj, PATH.PLAYER_API_PATH);
     });
   });
 
-  loadData(DATA_TEAMS_PATH).then(function (dataArr) {
+  loadData(PATH.DATA_TEAMS_PATH).then(function (dataArr) {
     dataArr.forEach(async function (team) {
       const teamObj = {
         //as "id" will have the default auto increment (1 - 25)
@@ -31,7 +28,7 @@ const controlData = function () {
         city: team.city,
         badge: "Undefined",
       };
-      await postToDB(teamObj, TEAM_API_PATH);
+      await postToDB(teamObj, PATH.TEAM_API_PATH);
     });
   });
 
