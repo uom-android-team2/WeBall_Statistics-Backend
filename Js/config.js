b023dfcc36e229627973f4b4417927fece8886ed
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-06-10 15:16:02 +0300

    created team and player lcompleted stats table

--- Js/config.js
+++ Js/config.js
@@ -1,4 +1,10 @@
 export const DATA_PLAYERS_PATH = "../resources/data/player_data/players.json";
 export const DATA_TEAMS_PATH = "../resources/data/team_data/teams.json";
-export const PLAYER_API_PATH = "http://localhost/WeBall_Statistics-Backend/API/player.php";
-export const TEAM_API_PATH = "http://localhost/WeBall_Statistics-Backend/API/team.php";
+export const PLAYER_API_PATH =
+  "http://localhost/WeBall_Statistics-Backend/API/player.php";
+export const TEAM_API_PATH =
+  "http://localhost/WeBall_Statistics-Backend/API/team.php";
+export const TEAM_COMPLETED_STATISTICS =
+  "http://localhost/WeBall_Statistics-Backend/API/teamStatisticsCompleted.php";
+export const PLAYER_COMPLETED_STATISTICS =
+  "http://localhost/WeBall_Statistics-Backend/API/playerStatisticsCompleted.php";
--- Js/controller.js
+++ Js/controller.js
@@ -4,8 +4,8 @@ import { postToDB } from "./Services/storeToDB.js";
 
 const btnLoadData = document.querySelector("#btn-load-data");
 
-const controlData = function () {
-  loadData(PATH.DATA_PLAYERS_PATH).then(function (dataArr) {
+const controlData = async function () {
+  await loadData(PATH.DATA_PLAYERS_PATH).then(function (dataArr) {
     dataArr.forEach(async function (player) {
       const playerObj = {
         //as "id" will have the default auto increment
@@ -20,7 +20,7 @@ const controlData = function () {
     });
   });
 
-  loadData(PATH.DATA_TEAMS_PATH).then(function (dataArr) {
+  await loadData(PATH.DATA_TEAMS_PATH).then(function (dataArr) {
     dataArr.forEach(async function (team) {
       const teamObj = {
         //as "id" will have the default auto increment (1 - 8)
@@ -31,11 +31,73 @@ const controlData = function () {
       await postToDB(teamObj, PATH.TEAM_API_PATH);
     });
   });
-
   alert("Successful registration of data in the database!");
 };
 
-btnLoadData.addEventListener("click", function (evt) {
+const createTeamStatisticsTable = async () => {
+  const res = await fetch(
+    "http://localhost/WeBall_Statistics-Backend/API/team.php"
+  );
+
+  const teams = await res.json();
+  teams.forEach(async (team) => {
+    const teamCompletedStats = {
+      team_id: team.id,
+      total_matches: "0",
+      win: "0",
+      lose: "0",
+      successful_effort: "0",
+      total_effort: "0",
+      successful_freethrow: "0",
+      total_freethrow: "0",
+      successful_twopointer: "0",
+      total_twopointer: "0",
+      successful_threepointer: "0",
+      total_threepointer: "0",
+      steal: "0",
+      assist: "0",
+      block: "0",
+      rebound: "0",
+      foul: "0",
+      turnover: "0",
+    };
+    await postToDB(teamCompletedStats, PATH.TEAM_COMPLETED_STATISTICS);
+  });
+};
+
+const createPlayerStatisticsTable = async () => {
+  const res = await fetch(
+    "http://localhost/WeBall_Statistics-Backend/API/player.php"
+  );
+
+  const players = await res.json();
+  players.forEach(async (player) => {
+    const playerCompletedStats = {
+      player_id: player.id,
+      matches_played: "0",
+      successful_effort: "0",
+      total_effort: "0",
+      successful_freethrow: "0",
+      total_freethrow: "0",
+      successful_twopointer: "0",
+      total_twopointer: "0",
+      successful_threepointer: "0",
+      total_threepointer: "0",
+      steal: "0",
+      assist: "0",
+      block: "0",
+      rebound: "0",
+      foul: "0",
+      turnover: "0",
+      minutes: "0",
+    };
+    await postToDB(playerCompletedStats, PATH.PLAYER_COMPLETED_STATISTICS);
+  });
+};
+
+btnLoadData.addEventListener("click", async function (evt) {
   evt.preventDefault();
-  controlData();
+  await controlData();
+  await createTeamStatisticsTable();
+  await createPlayerStatisticsTable();
 });
